ab1b1f0e90edd5e77187fa2702df90a524eef42e
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-27 23:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 
 function alerrt() {
+	event.preventDefault().
     alert(`First Name: ${document.querySelector('#first').value} Last Name: ${document.querySelector('#last').value} Phone Number: ${document.querySelector('#number').value} Email ID: ${document.querySelector('#email').value}`);
 }
