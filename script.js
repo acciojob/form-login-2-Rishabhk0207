6c049605a0e928ef31c1dd5110a74dfa9a244833
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-27 23:08:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+
+function alerrt() {
+    alert(`First Name: ${document.querySelector('#first').value} Last Name: ${document.querySelector('#last').value} Phone Number: ${document.querySelector('#number').value} Email ID: ${document.querySelector('#email').value}`);
+}
